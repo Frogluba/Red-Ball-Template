66c4890495a469202ccf22d18b40a010f995a0c3
7700s <146203655+Frogluba@users.noreply.github.com>
2023-11-15 20:03:54 +0200

    1level

---
+++ Assets/Ball.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float jumpSpeed;
+    public float moveForce;
+    Rigidbody2D rb;
+
+     void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        var hor = Input.GetAxisRaw("Horizontal");
+        rb.AddForce(new Vector2(hor, 0) * moveForce);
+
+     if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity += Vector2.up * jumpSpeed;
+  
+        }
+    }
+}
