88590496dbcfc4e8b17b2aab526507b59e9d8e69
Frogluba <146203655+Frogluba@users.noreply.github.com>
2023-11-19 12:43:40 +0200

    player fixed

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -7,6 +7,8 @@ public class Ball : MonoBehaviour
     public float jumpSpeed;
     public float moveForce;
     Rigidbody2D rb;
+    public float maxSpeed;
+    bool isGrounded;
 
      void Start()
     {
@@ -17,11 +19,33 @@ public class Ball : MonoBehaviour
     {
         var hor = Input.GetAxisRaw("Horizontal");
         rb.AddForce(new Vector2(hor, 0) * moveForce);
-
-     if (Input.GetButtonDown("Jump"))
+     //jump
+     if (Input.GetButtonDown("Jump")&& isGrounded)
         {
             rb.velocity += Vector2.up * jumpSpeed;
   
         }
+
+        
+     //limit speed
+        if(rb.velocity.magnitude > maxSpeed)
+        {
+            rb.drag = 1;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
     }
+
+     void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+
+   void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+
 }
